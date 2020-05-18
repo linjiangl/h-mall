312fe9bd6c349e2e7d9f3fd2823c0e397c11342c
linjiangl <8257796@qq.com>
2020-05-18 22:37:55 +0800

    单元测试脚本

--- script/phpunit/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker exec dhyperf bash -c "
-cd /hyperf-skeleton/h-mall
-rm -rf runtime/container
-bin/hyperf.php migrate:fresh --seed
-vendor/bin/co-phpunit -c phpunit.xml --colors=always"
