667faf8adad7e166b082080c687006a33decd337
jianglin <8257796@qq.com>
2020-07-01 15:43:41 +0800

    模型缓存配置

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -35,6 +35,7 @@ return [
             'ttl' => 3600 * 24,
             'empty_model_ttl' => 600,
             'load_script' => true,
+            'use_default_value' => true
         ],
         'commands' => [
             'gen:model' => [
