76a4c6220c901ad654b9458eb33c562c0c7b8917
jianglin <8257796@qq.com>
2021-05-09 14:12:18 +0800

    路由配置

---
+++ config/routes/app.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Multi-user mall
+ *
+ * @link     https://store.yii.red
+ * @document https://document.store.yii.red
+ * @contact  8257796@qq.com
+ */
+use App\Controller\App\v1\HomeController;
+use Hyperf\HttpServer\Router\Router;
+
+// 手机端接口路由
+Router::addGroup('/v1', function () {
+    // 首页
+    Router::get('/home', [HomeController::class, 'index']);
+});
