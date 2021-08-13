928b739d530513db03b6240a80b390592b6ba467
jianglin <8257796@qq.com>
2021-08-13 16:35:40 +0800

    改成php8 语法

--- app/Core/Helper/Functions.php
+++ app/Core/Helper/Functions.php
@@ -94,9 +94,8 @@ if (! function_exists('response')) {
 if (! function_exists('response_json')) {
     /**
      * 接口响应数据格式.
-     * @param mixed $data
      */
-    function response_json($data, string $message = '', int $code = 200): Psr\Http\Message\ResponseInterface
+    function response_json(mixed $data, string $message = '', int $code = 200): Psr\Http\Message\ResponseInterface
     {
         $code = $code ?: 500;
         if ($code >= 200 && $code < 300) {
