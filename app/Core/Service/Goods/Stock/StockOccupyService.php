fd3c67f56da596bba782361812a407fe41ad64a4
linjiangl <8257796@qq.com>
2021-09-24 17:08:01 +0800

    库存业务

--- app/Core/Service/Goods/Stock/StockOccupyService.php
+++ app/Core/Service/Goods/Stock/StockOccupyService.php
@@ -12,4 +12,28 @@ namespace App\Core\Service\Goods\Stock;
 
 class StockOccupyService
 {
+    /**
+     * 新增库存占用记录
+     */
+    public static function create(int $shopId, int $goodsId, string $module, int $moduleId, array $skus, string $serialNo = '', string $remark = '')
+    {
+
+    }
+
+    /**
+     * 更新库存占用记录
+     */
+    public static function update(int $shopId, int $goodsId, string $module, int $moduleId, array $skus, string $serialNo = '', string $remark = '')
+    {
+        self::destroy($shopId, $module, $moduleId);
+        self::create($shopId, $goodsId, $module, $moduleId, $skus, $serialNo, $remark);
+    }
+
+    /**
+     * 删除库存占用记录
+     */
+    public static function destroy(int $shopId, string $module, int $moduleId)
+    {
+
+    }
 }
